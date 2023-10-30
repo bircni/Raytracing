8b34d7444c702639c1b36c4fe2b0865bb19e311a
Deniz <karagoez@hs-ulm.de>
2023-10-30 12:30:40 +0100

    TRY-33 modified settings.rs to match the config.yaml structure

--- src/scene/settings.rs
+++ src/scene/settings.rs
@@ -1,5 +1,40 @@
+use serde::Deserialize;
+
 #[derive(Debug, Clone, PartialEq)]
-pub struct Settings {
+struct ModelConfig {
+    filePath: String,
+    position: Point3<f32>,
+    rotation: Vector3<f32>,
+    scale: f32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct PointLightConfig {
+    position: Point3<f32>,
+    Ke: Color,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct CameraConfig {
+    position: Point3<f32>,
+    lookAt: Point3<f32>,
+    upVec: Vector3<f32>,
+    fieldOfView: f32,
+    width: u32,
+    height: u32,
+}
+    
+#[derive(Debug, Clone, PartialEq)]
+struct ExtraArgsConfig {
     max_bounces: u32,
     samples: u32,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct AppConfig {
+    title: String,
+    model: ModelConfig,
+    pointLights: Vec<PointLightConfig>,
+    camera: CameraConfig,
+    extraArgs: ExtraArgsConfig,
 }
