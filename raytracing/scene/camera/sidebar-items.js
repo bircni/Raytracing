eec4f42e313404d140208d9f0b6ec7fc03078eff
bircni <bircni@users.noreply.github.com>
2023-11-02 12:57:52 +0000

    deploy: 535605070e2e3a9fdd14105513387e51124be85a

--- raytracing/scene/camera/sidebar-items.js
+++ raytracing/scene/camera/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["yaml"],"struct":["Camera"]};
+window.SIDEBAR_ITEMS = {"struct":["Camera"]};
--- raytracing/scene/yaml/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["color_rgb","point3_xyz","vector3_xyz"]};
