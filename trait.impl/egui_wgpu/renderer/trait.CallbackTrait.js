6184a710a90d4861fe539e690783ef7c02bd1944
Fabus1184 <Fabus1184@users.noreply.github.com>
2024-01-02 22:14:15 +0000

    deploy: 42cedb3b1f17c2eb189c56c9cf7faf118b177fc4

---
+++ trait.impl/egui_wgpu/renderer/trait.CallbackTrait.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"raytracing":[["impl CallbackTrait for <a class=\"struct\" href=\"raytracing/ui/preview/struct.PreviewRenderer.html\" title=\"struct raytracing::ui::preview::PreviewRenderer\">PreviewRenderer</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ type.impl/nalgebra/base/alias/type.Vector3.js
@@ -0,0 +1,3 @@
+(function() {var type_impls = {
+"raytracing":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
