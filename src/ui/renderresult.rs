ff38f3919832e4dccb885ca10b13dbbc4348ba21
Deniz <95908995+HermanoGranger@users.noreply.github.com>
2024-01-14 15:39:08 +0100

    TRY-107 Fix scrolling bug when export dialog has focus (#49)

--- src/ui/renderresult.rs
+++ src/ui/renderresult.rs
@@ -23,12 +23,15 @@ impl RenderResult {
 
             let response = response.on_hover_and_drag_cursor(CursorIcon::Grab);
 
-            self.zoom += ui.input(|i| i.scroll_delta.y);
-            self.zoom = self.zoom.clamp(
-                -response.rect.width().min(response.rect.height()) / 4.0,
-                std::f32::INFINITY,
-            );
-            self.position += response.drag_delta();
+            // Check if the dialog is being hovered over or active
+            if response.has_focus() || response.hovered() {
+                self.zoom += ui.input(|i| i.scroll_delta.y);
+                self.zoom = self.zoom.clamp(
+                    -response.rect.width().min(response.rect.height()) / 4.0,
+                    std::f32::INFINITY,
+                );
+                self.position += response.drag_delta();
+            }
 
             response.double_clicked().then(|| {
                 self.zoom = 0.0;
