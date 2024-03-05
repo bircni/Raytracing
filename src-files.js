851d24646b636aed20d2b1707c1ab41a54dfb659
bircni <bircni@users.noreply.github.com>
2024-03-05 15:55:15 +0000

    deploy: 83b531f168e0264ae6f0a880b340a732647b8033

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"raytracing":["",[["raytracer",[],["mod.rs","render.rs"]],["scene",[],["camera.rs","light.rs","material.rs","mod.rs","object.rs","settings.rs","skybox.rs","triangle.rs","yaml.rs"]],["ui",[["preview",[],["gpu.rs","mod.rs"]]],["mod.rs","properties.rs","renderresult.rs","statusbar.rs","yamlmenu.rs"]]],["main.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["raytracing",["",[["raytracer",[],["mod.rs","render.rs"]],["scene",[],["camera.rs","light.rs","material.rs","mod.rs","object.rs","settings.rs","skybox.rs","triangle.rs","yaml.rs"]],["ui",[["preview",[],["gpu.rs","mod.rs"]]],["mod.rs","properties.rs","renderresult.rs","statusbar.rs","yamlmenu.rs"]]],["main.rs"]]]\
+]'));
 createSrcSidebar();
