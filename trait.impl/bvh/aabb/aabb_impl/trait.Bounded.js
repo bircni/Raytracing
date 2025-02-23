2b71bcc537bfa29a7695c245ed2409fb8f420a7b
bircni <bircni@users.noreply.github.com>
2025-02-23 16:07:57 +0000

    deploy: fc1b071e572fb2ddafc5cb0946018acbaa943d7e

---
+++ trait.impl/bvh/aabb/aabb_impl/trait.Bounded.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["trayracer",[["impl Bounded&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.f32.html\">f32</a>, 3&gt; for <a class=\"struct\" href=\"trayracer/scene/triangle/struct.Triangle.html\" title=\"struct trayracer::scene::triangle::Triangle\">Triangle</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[284]}
--- trait.impl/bvh/bounding_hierarchy/trait.BHShape.js
+++ trait.impl/bvh/bounding_hierarchy/trait.BHShape.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["trayracer",[["impl BHShape&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/std/primitive.f32.html\">f32</a>, 3&gt; for <a class=\"struct\" href=\"trayracer/scene/triangle/struct.Triangle.html\" title=\"struct trayracer::scene::triangle::Triangle\">Triangle</a>"]]]]);
+    var implementors = Object.fromEntries([["trayracer",[["impl BHShape&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.f32.html\">f32</a>, 3&gt; for <a class=\"struct\" href=\"trayracer/scene/triangle/struct.Triangle.html\" title=\"struct trayracer::scene::triangle::Triangle\">Triangle</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
