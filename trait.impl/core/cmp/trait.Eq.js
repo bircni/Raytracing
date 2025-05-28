7406ab487fdc8766f845277caaf798ba6162e84c
bircni <bircni@users.noreply.github.com>
2025-05-28 21:16:41 +0000

    deploy: 8f68bc50ddf35813c6731ba96c74b06a811f691b

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["trayracer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"trayracer/scene/material/struct.IlluminationModel.html\" title=\"struct trayracer::scene::material::IlluminationModel\">IlluminationModel</a>"]]]]);
+    var implementors = Object.fromEntries([["trayracer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"trayracer/scene/material/struct.IlluminationModel.html\" title=\"struct trayracer::scene::material::IlluminationModel\">IlluminationModel</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
